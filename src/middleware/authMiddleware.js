24cf57c24f92c49f6f51633579a7d477fdc43ff6
Dimitar Yankulov <dimitarqnkulow@gmail.com>
2023-06-10 15:26:38 +0300

    Add auth middleware

---
+++ src/middleware/authMiddleware.js
@@ -0,0 +1,19 @@
+const jwt = require("../lib/jwt");
+const { SECRET } = require("../config/config");
+
+exports.auth = async (req, res, next) => {
+  const token = req.cookies["auth"];
+  console.log(token);
+  if (token) {
+    try {
+      const decodedToken = await jwt.verify(token, SECRET);
+      req.user = decodedToken;
+      next();
+    } catch (error) {
+      res.clearCookie("auth");
+      return res.redirect("/users/login");
+    }
+  } else {
+    next();
+  }
+};
